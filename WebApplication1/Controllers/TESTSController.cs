e1fb5d0707887e794031789a287620b1d43a4d48
TumTummai <tumtummai@MacBook-Air-khxng-TumTummai.local>
2021-07-12 11:24:20 +0700

    first commit

---
+++ WebApplication1/Controllers/TESTSController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DBContexts;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TESTSController : ControllerBase
+    {
+        private MyDBContext myDbContext;
+
+        public TESTSController(MyDBContext context)
+        {
+            myDbContext = context;
+        }
+
+        [HttpGet]
+        public IList<TESTS> Get()
+        {
+            return (this.myDbContext.TEST.ToList());
+        }
+    }
+}
---
+++ WebApplication1/DBContexts/MyDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DBContexts
+{
+    public class MyDBContext : DbContext
+    {
+        public DbSet<TESTS> TEST { get; set; }
+
+
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map entities to tables  
+            modelBuilder.Entity<TESTS>().ToTable("TEST");
+
+            // Configure Primary Keys  
+            modelBuilder.Entity<TESTS>().HasKey(u => u.id).HasName("PK_TEST");
+
+
+            // Configure columns  
+            modelBuilder.Entity<TESTS>().Property(u => u.id).HasColumnType("int").UseMySqlIdentityColumn().IsRequired();
+            modelBuilder.Entity<TESTS>().Property(u => u.date).HasColumnType("datetime").IsRequired();
+            modelBuilder.Entity<TESTS>().Property(u => u.stus).HasColumnType("boolean").IsRequired();
+            modelBuilder.Entity<TESTS>().Property(u => u.Des).HasColumnType("varchar(250)").IsRequired();
+
+         
+
+        }
+
+    }
+}
